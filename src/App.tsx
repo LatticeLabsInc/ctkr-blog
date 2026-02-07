2c948d6f38e72b62367fa5ebd42381cbdde47738
llchristopher <christopher@latticelabs.org>
2026-02-06 17:15:25 -0800

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <main className="container">
+      <h1>ctkr-blog</h1>
+      <p>Hello world! Your blog scaffold is ready.</p>
+    </main>
+  );
+};
+
+export default App;
